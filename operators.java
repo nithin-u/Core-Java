2e6d358daf0593ebb0dc87b4f2b3cd4ae6206671
nithin-u <nitinu522@gmail.com>
2024-01-27 16:58:14 +0530

    code completed

--- operators.java
+++ operators.java
@@ -1,9 +1,25 @@
 class operators {
     public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        // Arithematic Operators//
         System.out.println(10 + 20);
         System.out.println(100 - 20);
         System.out.println(10 * 20);
         System.out.println(100 / 20);
         System.out.println(10 % 2);
+        // Relational Operators//
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+        // Logical Operators //
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || true);
+        System.out.println(false || false);
+
     }
 }
