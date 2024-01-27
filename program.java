64685e7cb7ee69c34a7dbe7b7fad0fdfbc8ead14
nithin-u <nitinu522@gmail.com>
2024-01-27 15:21:01 +0530

    Bug fixed

--- program.java
+++ program.java
@@ -1,7 +1,5 @@
-class program 
-{
-  public static void main(String []args)
-  {
-    System.out.println("Hello there! it's me JAVA")
+class program {
+  public static void main(String[] args) {
+    System.out.println("Hello there! it's me JAVA");
   }
 }
